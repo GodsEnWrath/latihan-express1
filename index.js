8830847545599ecd1f38ca9ad24ad602edad0cbb
GodsEnWrath <fastyazid@gmail.com>
2019-11-26 16:45:45 +0700

    little fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
 const cors = require("cors")
-const PORT = process.eventNames.PORT || 5000
+const PORT = process.env.PORT || 5000
 
 
 app.use(cors())
